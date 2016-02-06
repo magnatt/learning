c4c8c0696f4dac2b47899997ec79bdaf408683db
tatarinZanton <tatarin.com@gmail.com>
2016-02-06 09:58:20 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+	<title>Мой первый скрипт</title>
+</head>
+
+<body 
+<?php
+echo "Hello World";
+?>
+>
+<?php
+$foo1 = 15;
+?>
+<?php
+/*$testValue = 5;
+$testValue %=5;
+
+echo $testValue;*/
+
+?>
+
+</body>
+
+</html>
